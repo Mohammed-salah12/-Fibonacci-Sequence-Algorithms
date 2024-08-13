a1c950d1844a14c20429c58b9aca0f0ac5d33507
Mohammed-salah12 <mohammedsalah899000@gmail.com>
2024-08-13 13:51:27 +0300

    Fibonacci Algorithm

--- practice.js
+++ practice.js
@@ -1,10 +1,10 @@
-function fibnatchy(n) {
+function Fibonacci(n) {
   let fib = [0, 1];
   for (let i = 2; i < n; i++) {
     fib[i] = fib[i - 1] + fib[i - 2];
   }
   return fib;
 }
-console.log(fibnatchy(7));
+console.log(Fibonacci(7));
 
 // big-O  = O(n) linear
