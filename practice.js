29a5fc56638a7d628f28963c0ee3ab402e128ec4
Mohammed-salah12 <mohammedsalah899000@gmail.com>
2024-08-13 13:48:24 +0300

    init

---
+++ practice.js
@@ -0,0 +1,10 @@
+function fibnatchy(n) {
+  let fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+}
+console.log(fibnatchy(7));
+
+// big-O  = O(n) linear
